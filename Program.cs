458d0417c4d7ea093adfafe73b2631e77c0f8877
Gihun Ham <progh2@gmail.com>
2025-05-28 10:17:53 +0900

    생성자,  기본 생성자 연습

--- Program.cs
+++ Program.cs
@@ -11,10 +11,16 @@ namespace PropExample
         static void Main(string[] args)
         {
             TestOver(3L);
+
+            // 생성자
+            //Product p = new Product();  // 기본생성자(기정생성자)는 생성자가 없을 경우에만 자동으로 생성
+            Product p = new Product("아아", 1500);
+
         }
 
-        // 오버로딩 
+        // 오버로딩 주의점 
         //public static int TestOver(int input) { return 0;  }
         public static bool TestOver(float input) { return true; }
     }
+
 }
