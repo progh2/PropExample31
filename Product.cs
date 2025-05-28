9d19e481fc88c75e80896cdf2c8b2d673da89212
Gihun Ham <progh2@gmail.com>
2025-05-28 10:21:35 +0900

    팩토리 메서드 패턴 - private 생성자 사용

--- Product.cs
+++ Product.cs
@@ -11,8 +11,13 @@ namespace PropExample
         public string name;
         public int price;
 
+        // 팩토리 메서드 패턴
+        public static Product getInstance(string name, int price )
+        {
+            return new Product(name, price);
+        }
 
-        public Product(string name, int price)
+        private Product(string name, int price)
         {
             this.name = name;
             this.price = price;
--- Program.cs
+++ Program.cs
@@ -14,7 +14,9 @@ namespace PropExample
 
             // 생성자
             //Product p = new Product();  // 기본생성자(기정생성자)는 생성자가 없을 경우에만 자동으로 생성
-            Product p = new Product("아아", 1500);
+            //Product p = new Product("아아", 1500);
+            // 팩토리 메서드 패턴 - private 생성자 사용 
+            Product p = Product.getInstance("뜨아", 1000);
 
         }
 
